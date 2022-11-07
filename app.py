9403e29b226ceec7addc229c5ba491b476740c7b
JohnAyo2005 <116895781+JohnAyo2005@users.noreply.github.com>
2022-11-07 08:45:29 +0100

    Update and rename index.py to app.py

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, request, json, Response
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app, resources={r"*": {"origins": "*"}})
+
+
+@app.route('/stage2/', methods=['POST'])
+def operation():
+    if request.method == 'POST':
+        operation = request.json["operation_type"]
+        x = request.json['x']
+        y = request.json['y']
+        slackUsername = 'Johnny2005'
+
+        if operation == 'addition':
+            sum = x + y
+            data = { "slackUsername": slackUsername, "result": sum, "operation_type" : operation }
+
+            response = json.dumps(data, sort_keys=False)
+            return Response(response, mimetype='application/json')
+
+        elif operation == 'subtraction':
+            minus = x - y
+            data = { "slackUsername": slackUsername, "result": minus, "operation_type" : operation }
+
+            response = json.dumps(data, sort_keys=False)
+            return Response(response, mimetype='application/json')
+
+        elif operation == 'multiplication':
+            times = x * y
+            data = { "slackUsername": slackUsername, "result": times, "operation_type" : operation }
+
+            response = json.dumps(data, sort_keys=False)
+            return Response(response, mimetype='application/json')
+        else:
+            return {"response": "Operation must be either addition, multiplication or Subtration"}
+    
+    return "error processing reqeust :("
+
+
+if __name__ == '__main__':
+    app.run(debug=True
